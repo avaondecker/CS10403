4b8d2870213b907a10ed76a682156f48f2246db3
avaondecker <91210480+avaondecker@users.noreply.github.com>
2021-10-04 12:30:03 -0400

    Average.py

---
+++ Average.py
@@ -0,0 +1,23 @@
+#Ava Ondecker
+#score average program with while loop
+avg = 0
+total = 0
+howManyEntered = 0
+
+howMany = int(input('How many test scores would you like to average? '))
+
+while(howManyEntered < howMany):
+
+    testScore = int(input('Enter test score: '))
+
+    total += testScore
+
+    howManyEntered += 1
+
+avg = total/howMany
+
+print('The average for the test scores you entered is:' , avg)
+
+
+    
+    
