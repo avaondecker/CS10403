7f23ff6d8dce6b6dcb8025dabd18812722a61bd7
avaondecker <91210480+avaondecker@users.noreply.github.com>
2021-11-29 11:10:28 -0500

    PythonQueue

---
+++ PythonQueue.py
@@ -0,0 +1,13 @@
+#Ava Ondecker
+#PythonQueue Program
+#Professor Eckert
+#11/29/21
+
+names= []
+for j in range(10):
+    name = input('Enter a customer name: ')
+    names.append(name)
+
+for j in range(10):
+    len(names)
+    print(names.pop(0))
